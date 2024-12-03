3763bcd494028c1b7eaa7fa47d45a4736fbf35f5
VenuMadhav Sheshabhattar <vsheshabhattar210@everestdx.com>
2024-12-03 23:27:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm [Your Name]. This is my portfolio website.</p>
+      <a href="https://www.linkedin.com/in/yourprofile/" target="_blank" rel="noopener noreferrer">
+        LinkedIn
+      </a>
     </div>
   );
 }
-
 export default App;
